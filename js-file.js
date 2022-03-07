bae62a6e6d12c0ad56b823b576cb62edfb75f80a
rlndgg <rlndgg@gmail.com>
2022-03-07 11:51:22 -0600

    center items, removed blockdiv class

--- js-file.js
+++ js-file.js
@@ -8,11 +8,8 @@ for (let i = 0; i < 16 ; i++) {
 
     content.classList.add('flexdiv');
 
-    if ( i+1 % 4 == 0) {
-        content.classList.add('blockbox');
-
-    }
     content.textContent = `${i+1}`;
+    
 
     container.appendChild(content);
 }
