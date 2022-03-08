7aed3b550c594b50112118c07bb12505061c5bd7
rlndgg <rlndgg@gmail.com>
2022-03-08 12:29:43 -0600

    added createGrid, to add a dynamic div grid subject to the input of the user

--- js-file.js
+++ js-file.js
@@ -8,7 +8,7 @@ for (let i = 0; i < 16 ; i++) {
 
     content.classList.add('flexdiv');
 
-    content.textContent = `${i+1}`;
+  //  content.textContent = `${i+1}`;
     
 
     container.appendChild(content);
@@ -23,8 +23,78 @@ containers.forEach( (container) => {
         container.classList.add('mouseoverdiv');   
     });
 
-    container.addEventListener('mouseout', () => {
+
+});
+
+function prmptUser () {
+    let rowsAsk = window.prompt("How many rows will the grid have? (limit: 100)",'10');
+    let columnAsk = window.prompt("How many columns will the grid have? (limit: 100)",'10');
+    
+    const container = document.querySelector('#container');
+    if (container !== null) { container.remove();};
+
+    createGrid(rowsAsk,columnAsk);
+    return;
+}
+
+const button = document.querySelector('button');
+    let returnArray = Array(2);
+    button.addEventListener("click",prmptUser);
+
+function createGrid (rows,columns) {
+    if (  document.querySelector('#newContainer') !== null) {
+        const newContainer = document.querySelector('#newContainer');
+        newContainer.remove(); }
+
+    const container = document.createElement('div');
+    container.setAttribute('id','newContainer');
+    document.body.appendChild(container);
+    let counter = 1;
+
+    for (let j = 0; j < rows; j++){
+        const row = document.createElement('div');
+        row.classList.add('row');
+        
+        
+        for (let k = 0; k < columns; k++){
+
+            const cell = document.createElement("div");
+            cell.classList.add('griditem');
+            
+            cell.addEventListener('mouseover', () => {
+
+                cell.classList.add('mouseoverdiv');   
+            });
+
+
+ //           cell.textContent = `${counter}`;
+            counter++
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
+    }
+  
+
+};
+
+
+
+/*
+    const container = document.querySelector('#container');
+
+    for (let i = 0; i < returnArray[1] ; i++) {
+    
+        const content = document.createElement('div');
+    
+        content.classList.add('flexdiv');
+    
+        content.textContent = `${i+1}`;
+        
+    
+        container.appendChild(content);
+    }
+
+   /* container.addEventListener('mouseout', () => {
 
         container.classList.remove('mouseoverdiv');   
-    });
-});
+    }); */
